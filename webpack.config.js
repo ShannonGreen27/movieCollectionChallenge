bb27ae229b4b9d43c92cef2f4db0db0e81ec2ca3
ShannonGreen27 <shannon.jm.green@gmail.com>
2017-04-26 23:37:52 -0500

    created server file. package.json and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const DIST_DIR = path.resolve(__dirname, "./app.js")
+const DIST_SRC = path.resolve(__dirname, "public")
+
+module.exports =  {
+	entry: DIST_DIR,
+	output: {
+		path: DIST_SRC,
+		filename: 'bundle.js',
+		publicPath: "/"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+        		exclude: /node_modules/,
+				loader: "babel-loader",
+				query: {
+					presets: ["es2015", "react"]
+				}
+			}
+		]
+	},
+	// This lets us debug our react code in chrome dev tools. Errors will have lines and file names
+	// Without this the console says all errors are coming from just coming from bundle.js
+	devtool: "eval-source-map"
+}
